cbade1814564180ea7add0e54b08818150398a26
Emerson Cloud <emerson.cloud@gmail.com>
2021-12-11 20:43:34 -0500

    rocket boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+use rocket::{Rocket, Build};
+use rocket::tokio::time::{sleep, Duration};
+use rocket::tokio::task::spawn_blocking;
+
+use std::io;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello world!"
 }
+
+#[get("/delay/<seconds>")]
+async fn delay(seconds: u64) -> String {
+    sleep(Duration::from_secs(seconds)).await;
+    format!("Waited for {} seconds", seconds)
+}
+
+#[get("/blocking_task")]
+async fn blocking_task() -> io::Result<Vec<u8>> {
+    let vec = spawn_blocking(|| std::fs::read("data.txt")).await
+        .map_err(|e| io::Error::new(io::ErrorKind::Interrupted, e))??;
+    Ok(vec)
+}
+
+#[launch]
+fn rocket() -> Rocket<Build> {
+    rocket::build()
+        .mount("/", routes![index, delay, blocking_task])
+        .mount("/hi", routes![index])
+}
+
+// #[rocket::main]
+// async fn main() {
+//     rocket::build()
+//         .mount("/hello", routes![index])
+//         .launch()
+//         .await;
+// }
