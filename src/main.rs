9001c87f9a8bb6844b0175e7b61f7ccd519454ce
Emerson Cloud <emerson.cloud@gmail.com>
2021-12-20 12:43:20 -0500

    Add post for contact form

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,60 @@
 #[macro_use] extern crate rocket;
 
+use std::borrow::Cow;
+
+use rocket::State;
+use rocket::tokio::sync::Mutex;
 use rocket::{Rocket, Build};
 use rocket::tokio::time::{sleep, Duration};
 use rocket::tokio::task::spawn_blocking;
+use rocket::serde::json::{Json, Value, json};
+use rocket::serde::{Serialize, Deserialize};
 
 use std::io;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello world!"
+type Id = usize;
+
+#[derive(Serialize, Deserialize)]
+#[derive(Clone)]
+#[serde(crate = "rocket::serde")]
+struct Jacky<'r> {
+    id: Option<Id>,
+    name: Cow<'r, str>,
+    email: Cow<'r, str>,
+    phone: Cow<'r, str>,
+    message: Cow<'r, str>
 }
 
-#[get("/delay/<seconds>")]
-async fn delay(seconds: u64) -> String {
-    sleep(Duration::from_secs(seconds)).await;
-    format!("Waited for {} seconds", seconds)
+type MessageList<'r> = Mutex<Vec<String>>;
+type Jackies<'r> = &'r State<MessageList<'r>>;
+
+#[post("/", format = "json", data = "<message>")]
+async fn new<'x>(message: Json<Jacky<'_>>, list: Jackies<'_>) -> Value {
+    let mut list = list.lock().await;
+    let id = list.len();
+    list.push(message.name.to_string());
+    // list.push(Jacky {
+    //     id: None,
+    //     name: message.name.clone(),
+    //     email: message.email.clone(),
+    //     phone: message.phone.clone(),
+    //     message: message.message.clone(),
+    //
+    // });
+
+    json!({"status": "ok", "id": id})
 }
 
-#[get("/blocking_task")]
-async fn blocking_task() -> io::Result<Vec<u8>> {
-    let vec = spawn_blocking(|| std::fs::read("data.txt")).await
-        .map_err(|e| io::Error::new(io::ErrorKind::Interrupted, e))??;
-    Ok(vec)
+#[get("/<id>", format = "json")]
+async fn get(id: Id, list: Jackies<'_>) -> Option<Json<String>> {
+    let list = list.lock().await;
+    let local_jack = list.get(id);
+    if let Some(j) = local_jack {
+        Some(Json(j.into()))
+    } else {
+        None
+    }
 }
-
-// #[get("/jacky/<input>")]
-// fn jacky(input: &RawStr) -> String {
-//     format!("{}{}", input.as_str(), "y")
-// }
 
 #[get("/jacky/<name>")]
 fn jacky(name: &str) -> String {
@@ -37,13 +64,6 @@ fn jacky(name: &str) -> String {
 #[launch]
 fn rocket() -> Rocket<Build> {
     rocket::build()
-        .mount("/", routes![index, delay, blocking_task, jacky])
+        .mount("/", routes![jacky, new, get])
+        .manage(MessageList::new(vec![]))
 }
-
-// #[rocket::main]
-// async fn main() {
-//     rocket::build()
-//         .mount("/hello", routes![index])
-//         .launch()
-//         .await;
-// }
