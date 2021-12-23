1ec9e95812d7b5bcebd50261ad4c1a498a2e456a
Emerson Cloud <emerson.cloud@gmail.com>
2021-12-22 19:54:09 -0500

    add env

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ use rocket::tokio::sync::Mutex;
 use rocket::{Rocket, Build};
 use rocket::serde::json::{Json, Value, json};
 use rocket::serde::{Serialize, Deserialize};
+use dotenv;
+use openapi::apis::{configuration::Configuration, default_api as twilio_api};
+use std::env;
 
 type Id = usize;
 
@@ -52,6 +55,48 @@ fn jacky(name: &str) -> String {
     format!("{}y", name)
 }
 
+#[get("/twil")]
+async fn twilio() {
+    dotenv::dotenv().expect("Failed to read .env file");
+    let account_sid = env::var("TWILIO_ACCOUNT_SID").unwrap();
+    let api_key = env::var("TWILIO_API_KEY").unwrap();
+    let api_key_sercret = env::var("TWILIO_API_KEY_SECRET").unwrap();
+    let from = env::var("TWILIO_PHONE_NUMBER").unwrap();
+    let to = env::var("TO_NUMBER").unwrap();
+
+    let mut twilio_config = Configuration::default();
+    twilio_config.basic_auth = Some((api_key, Some(api_key_sercret)));
+
+    let message = twilio_api::create_message(
+        &twilio_config,
+        &account_sid,
+        &to,
+        None,
+        None,
+        None,
+        Some("Ahoy, Rustacean! 🦀"),
+        None,
+        None,
+        Some(&from),
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+    )
+        .await;
+
+    let result = match message {
+        Ok(result) => result,
+        Err(error) => panic!("Something went wrong {:?}", error),
+    };
+    println!("{:?}", result.sid);
+}
+
 #[options("/")]
 fn good_jack() -> Value {
     json!({"hours": "8", "rate": "150.00", "payment_owed": "1200.00"})
@@ -60,6 +105,6 @@ fn good_jack() -> Value {
 #[launch]
 fn rocket() -> Rocket<Build> {
     rocket::build()
-        .mount("/", routes![jacky, new, get, get_all, good_jack])
+        .mount("/", routes![jacky, new, get, get_all, good_jack, twilio])
         .manage(ContactList::new(vec![]))
 }
